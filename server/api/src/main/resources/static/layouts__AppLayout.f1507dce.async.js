fd4d1aa3a838a328df4cd62ae4453609d7c0fd7c
javahuang <javahrp@gmail.com>
2022-09-19 08:23:50 +0800

    release: 😊 public v1.0.0

---
+++ server/api/src/main/resources/static/layouts__AppLayout.f1507dce.async.js
@@ -0,0 +1,1 @@
+(self.webpackChunksurvey_king=self.webpackChunksurvey_king||[]).push([[8076],{64724:function(v,u,n){"use strict";n.r(u),n.d(u,{AppLayout:function(){return r}});var t=n(23156),a=n(67294),e=n(85893),r=function(o){var s=(0,a.useMemo)(function(){return new t.ZP},[]);return(0,e.jsx)(t.Il.Provider,{value:s,children:o.children})};u.default=r}}]);
