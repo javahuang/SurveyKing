adb9e1f214866d0cbbfda800a9e61db6396db27a
javahuang <javahrp@gmail.com>
2022-09-23 08:38:00 +0800

    fix: 只允许查看自己创建的题库

--- server/rdbms/src/main/java/cn/surveyking/server/impl/RepoServiceImpl.java
+++ server/rdbms/src/main/java/cn/surveyking/server/impl/RepoServiceImpl.java
@@ -49,6 +49,7 @@ public class RepoServiceImpl extends BaseService<RepoMapper, Repo> implements Re
 	public PaginationResponse<RepoView> listRepo(RepoQuery query) {
 		Page<Repo> page = pageByQuery(query,
 				Wrappers.<Repo>lambdaQuery().like(isNotBlank(query.getName()), Repo::getName, query.getName())
+						.eq(Repo::getCreateAt, SecurityContextUtils.getUserId())
 						.eq(query.getMode() != null, Repo::getMode, query.getMode()).orderByAsc(Repo::getCreateAt));
 		PaginationResponse<RepoView> result = new PaginationResponse<>(page.getTotal(),
 				repoViewMapper.toView(page.getRecords()));
