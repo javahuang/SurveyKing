0769d953e1ce6444cafb4a900fab1578506c0bff
javahuang <javahrp@gmail.com>
2022-10-13 22:27:50 +0800

    feat: 优化扫码题识别率

--- server/shared/src/main/java/cn/surveyking/server/core/uitls/BarcodeReader.java
+++ server/shared/src/main/java/cn/surveyking/server/core/uitls/BarcodeReader.java
@@ -3,14 +3,15 @@ package cn.surveyking.server.core.uitls;
 import com.google.zxing.*;
 import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
 import com.google.zxing.common.HybridBinarizer;
-import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 import lombok.SneakyThrows;
+import net.coobird.thumbnailator.Thumbnails;
 
-import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.HashMap;
+import java.util.EnumMap;
+import java.util.EnumSet;
 import java.util.Map;
 
 /**
@@ -21,27 +22,37 @@ public class BarcodeReader {
 
 	@SneakyThrows
 	public static String readBarcode(InputStream inputStream) {
-		Map<EncodeHintType, ErrorCorrectionLevel> hintMap = new HashMap<EncodeHintType, ErrorCorrectionLevel>();
-		hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
-		String charset = "UTF-8"; // or "ISO-8859-1"
-		return readBarcode(inputStream, charset, hintMap);
+
+		Map<DecodeHintType, Object> hintMap = new EnumMap<DecodeHintType, Object>(DecodeHintType.class);
+		hintMap.put(DecodeHintType.CHARACTER_SET, "utf-8");
+		hintMap.put(DecodeHintType.TRY_HARDER, Boolean.TRUE);
+		hintMap.put(DecodeHintType.POSSIBLE_FORMATS, EnumSet.allOf(BarcodeFormat.class));
+
+		Map<DecodeHintType, Object> hintMap_pure = new EnumMap<DecodeHintType, Object>(DecodeHintType.class);
+		hintMap_pure.put(DecodeHintType.CHARACTER_SET, "utf-8");
+		hintMap_pure.put(DecodeHintType.TRY_HARDER, Boolean.TRUE);
+		hintMap_pure.put(DecodeHintType.PURE_BARCODE, Boolean.TRUE);
+		return readBarcode(inputStream, hintMap, hintMap_pure);
 	}
 
 	/**
 	 * @param inputStream
-	 * @param charset
 	 * @param hintMap
 	 * @return Qr Code value
 	 * @throws FileNotFoundException
 	 * @throws IOException
 	 * @throws NotFoundException
 	 */
-	public static String readBarcode(InputStream inputStream, String charset, Map hintMap)
+	public static String readBarcode(InputStream inputStream, Map hintMap, Map hintMap_pure)
 			throws FileNotFoundException, IOException, NotFoundException {
-		BinaryBitmap binaryBitmap = new BinaryBitmap(
-				new HybridBinarizer(new BufferedImageLuminanceSource(ImageIO.read(inputStream))));
+		BufferedImage image = Thumbnails.of(inputStream).size(1280, 960).asBufferedImage();
+		BinaryBitmap binaryBitmap = new BinaryBitmap(new HybridBinarizer(new BufferedImageLuminanceSource(image)));
 		Result qrCodeResult = new MultiFormatReader().decode(binaryBitmap, hintMap);
+		if (qrCodeResult == null) {
+			qrCodeResult = new MultiFormatReader().decode(binaryBitmap, hintMap_pure);
+		}
 		return qrCodeResult.getText();
+
 	}
 
 }
